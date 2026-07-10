7e229d7579b5cd2a2470626e803bd9b06d085dcd
Guhanshiva <guhanshiva@gmail.com>
2026-07-10 12:05:31 +0530

    updates

--- config/config.js
+++ config/config.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 
+const config = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+};
+
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  },
+  development: config,
+  production: config,
 };
